d6327d4995baf237fabce3ef8c84fcd1752ab149
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-04-10 17:24:56 -0300

    add Modal and Button components

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+// Component outdated because NextJS 13.3 doesn't need it anymore.
+
+import { useEffect, useState } from 'react';
+
+interface ClientOnlyProps {
+  children: React.ReactNode;
+}
+
+const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export default ClientOnly;
