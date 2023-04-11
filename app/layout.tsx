cf743258a5b50541c9cd9bd9c54b81678ab59c13
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-04-11 10:54:35 -0300

    sanitize user data to fix hydration issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
 
   return (
     <html lang='en'>
-      <body className={font.className}>
+      <body className={`${font.className} select-none`}>
         <ToasterProvider />
         <LoginModal />
         <RegisterModal />
