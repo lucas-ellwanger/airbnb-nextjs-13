f4dfbecb52ea984ed8964f0bfec4854bb058fbd7
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-04-11 16:23:56 -0300

    finish location step of rent modal

---
+++ app/components/inputs/CountrySelect.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+const flagemojiToPNG = (flag: string) => {
+  var countryCode = Array.from(flag, (codeUnit: any) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join('');
+  return (
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt='flag' />
+  );
+};
+
+import Select from 'react-select';
+
+import useCountries from '@/app/hooks/useCountries';
+
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: number[];
+  region: string;
+  value: string;
+};
+
+interface CountrySelectProps {
+  value?: CountrySelectValue;
+  onChange: (value: CountrySelectValue) => void;
+}
+
+const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
+  const { getAll } = useCountries();
+
+  return (
+    <div>
+      <Select
+        placeholder='Anywhere'
+        isClearable
+        options={getAll()}
+        value={value}
+        onChange={(value) => onChange(value as CountrySelectValue)}
+        formatOptionLabel={(option: any) => (
+          <div className='flex flex-row items-center gap-3'>
+            <>
+              <>{flagemojiToPNG(option.flag)}</>
+            </>
+            <div>
+              {option.label},
+              <span className='text-neutral-500 ml-1'>{option.region}</span>
+            </div>
+          </div>
+        )}
+        classNames={{
+          control: () => 'p-3 border-2',
+          input: () => 'text-lg',
+          option: () => 'text-lg',
+        }}
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 6,
+          colors: {
+            ...theme.colors,
+            primary: 'black',
+            primary25: '#ffe4e6',
+          },
+        })}
+      />
+    </div>
+  );
+};
+export default CountrySelect;
