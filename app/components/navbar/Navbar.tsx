3118a6eb36a95561c521e8d11cea837c0f1d8919
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-04-11 09:39:14 -0300

    add Login and Register hooks, get current user and change UI with user data

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+import { User } from '@prisma/client';
+
 import Container from '../Container';
 import Logo from './Logo';
 import Search from './Search';
 import UserMenu from './UserMenu';
 
-function Navbar() {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className='fixed w-full bg-white z-10 shadow-sm'>
       <div
@@ -27,11 +33,12 @@ function Navbar() {
           >
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
     </div>
   );
-}
+};
+
 export default Navbar;
