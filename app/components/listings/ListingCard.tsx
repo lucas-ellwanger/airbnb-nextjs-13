2a8e46752598c0e34ee2c235036afd52e8c0026d
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-04-11 21:19:16 -0300

    sanitize types for getListings

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { Listing, Reservation } from '@prisma/client';
+import { Reservation } from '@prisma/client';
 
-import { SafeUser } from '@/app/types';
+import { SafeListing, SafeUser } from '@/app/types';
+
 import useCountries from '@/app/hooks/useCountries';
 import { useCallback, useMemo } from 'react';
 import { format } from 'date-fns';
@@ -12,7 +13,7 @@ import HeartButton from '../HeartButton';
 import Button from '../Button';
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
