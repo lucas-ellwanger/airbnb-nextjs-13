b423e790a625b5e2db7c20a16e0a76274dbd0863
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-04-12 12:00:24 -0300

    disable dates in calendar that user already rented

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -2,19 +2,19 @@
 
 import axios from 'axios';
 import { useCallback, useEffect, useMemo, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { Range } from 'react-date-range';
 import { useRouter } from 'next/navigation';
-import { Reservation } from '@prisma/client';
-import { differenceInCalendarDays, eachDayOfInterval } from 'date-fns';
+import { differenceInDays, eachDayOfInterval } from 'date-fns';
 
-import { SafeListing, SafeUser } from '@/app/types';
-import { categories } from '@/app/components/navbar/Categories';
+import useLoginModal from '@/app/hooks/useLoginModal';
+import { SafeListing, SafeReservation, SafeUser } from '@/app/types';
+
 import Container from '@/app/components/Container';
+import { categories } from '@/app/components/navbar/Categories';
 import ListingHead from '@/app/components/listings/ListingHead';
 import ListingInfo from '@/app/components/listings/ListingInfo';
 import ListingReservation from '@/app/components/listings/ListingReservation';
-import useLoginModal from '@/app/hooks/useLoginModal';
-import { toast } from 'react-hot-toast';
-import { Range } from 'react-date-range';
 
 const initialDateRange = {
   startDate: new Date(),
@@ -23,7 +23,7 @@ const initialDateRange = {
 };
 
 interface ListingClientProps {
-  reservations?: Reservation[];
+  reservations?: SafeReservation[];
   listing: SafeListing & {
     user: SafeUser;
   };
@@ -41,17 +41,21 @@ const ListingClient: React.FC<ListingClientProps> = ({
   const disabledDates = useMemo(() => {
     let dates: Date[] = [];
 
-    reservations.forEach((reservation) => {
+    reservations.forEach((reservation: any) => {
       const range = eachDayOfInterval({
         start: new Date(reservation.startDate),
         end: new Date(reservation.endDate),
       });
 
-      dates = { ...dates, ...range };
+      dates = [...dates, ...range];
     });
 
     return dates;
   }, [reservations]);
+
+  const category = useMemo(() => {
+    return categories.find((items) => items.label === listing.category);
+  }, [listing.category]);
 
   const [isLoading, setIsLoading] = useState(false);
   const [totalPrice, setTotalPrice] = useState(listing.price);
@@ -61,7 +65,6 @@ const ListingClient: React.FC<ListingClientProps> = ({
     if (!currentUser) {
       return loginModal.onOpen();
     }
-
     setIsLoading(true);
 
     axios
@@ -74,11 +77,10 @@ const ListingClient: React.FC<ListingClientProps> = ({
       .then(() => {
         toast.success('Listing reserved!');
         setDateRange(initialDateRange);
-        // Redirect to /trips
-        router.refresh();
+        // router.push('/trips');
       })
       .catch(() => {
-        toast.error('Something went wrong');
+        toast.error('Something went wrong.');
       })
       .finally(() => {
         setIsLoading(false);
@@ -87,10 +89,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
 
   useEffect(() => {
     if (dateRange.startDate && dateRange.endDate) {
-      const dayCount = differenceInCalendarDays(
-        dateRange.endDate,
-        dateRange.startDate
-      );
+      const dayCount = differenceInDays(dateRange.endDate, dateRange.startDate);
 
       if (dayCount && listing.price) {
         setTotalPrice(dayCount * listing.price);
@@ -100,13 +99,14 @@ const ListingClient: React.FC<ListingClientProps> = ({
     }
   }, [dateRange, listing.price]);
 
-  const category = useMemo(() => {
-    return categories.find((item) => item.label === listing.category);
-  }, [listing.category]);
-
   return (
     <Container>
-      <div className='max-w-screen-lg mx-auto'>
+      <div
+        className='
+          max-w-screen-lg 
+          mx-auto
+        '
+      >
         <div className='flex flex-col gap-6'>
           <ListingHead
             title={listing.title}
@@ -117,12 +117,12 @@ const ListingClient: React.FC<ListingClientProps> = ({
           />
           <div
             className='
-            grid
-            grid-cols-1
-            md:grid-cols-7
-            md:gap-10
-            mt-6
-          '
+              grid 
+              grid-cols-1 
+              md:grid-cols-7 
+              md:gap-10 
+              mt-6
+            '
           >
             <ListingInfo
               user={listing.user}
@@ -135,9 +135,9 @@ const ListingClient: React.FC<ListingClientProps> = ({
             />
             <div
               className='
-                order-first
-                mb-10
-                md:order-last
+                order-first 
+                mb-10 
+                md:order-last 
                 md:col-span-3
               '
             >
@@ -157,4 +157,5 @@ const ListingClient: React.FC<ListingClientProps> = ({
     </Container>
   );
 };
+
 export default ListingClient;
--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,7 +1,10 @@
 import getCurrentUser from '@/app/actions/getCurrentUser';
 import getListingById from '@/app/actions/getListingById';
 import EmptyState from '@/app/components/EmptyState';
+
 import ListingClient from './ListingClient';
+import getReservations from '@/app/actions/getReservation';
+import ClientOnly from '@/app/components/ClientOnly';
 
 interface IParams {
   listingId?: string;
@@ -9,6 +12,7 @@ interface IParams {
 
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
@@ -16,9 +20,11 @@ const ListingPage = async ({ params }: { params: IParams }) => {
   }
 
   return (
-    <>
-      <ListingClient listing={listing} currentUser={currentUser} />
-    </>
+    <ListingClient
+      listing={listing}
+      reservations={reservations}
+      currentUser={currentUser}
+    />
   );
 };
 export default ListingPage;
