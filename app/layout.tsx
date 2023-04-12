bf4d9bade30eb8ccd563aed022f0f45d7145c286
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-04-12 16:27:04 -0300

    add search modal and finished filtering on search modal and category picker of home screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/navbar/Navbar';
 import RegisterModal from './components/modals/RegisterModal';
 import LoginModal from './components/modals/LoginModal';
 import RentModal from './components/modals/RentModal';
+import SearchModal from './components/modals/SearchModal';
 
 import ToasterProvider from './providers/ToasterProvider';
 import getCurrentUser from './actions/getCurrentUser';
@@ -29,6 +30,7 @@ export default async function RootLayout({
     <html lang='en'>
       <body className={`${font.className} select-none`}>
         <ToasterProvider />
+        <SearchModal />
         <RentModal />
         <LoginModal />
         <RegisterModal />
